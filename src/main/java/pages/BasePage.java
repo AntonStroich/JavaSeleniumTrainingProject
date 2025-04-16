b1f764ac442ef8d957649778b53074261f5ef984
Anton.Stroich <anton.stroich@ventionteams.com>
2025-04-16 18:03:23 +0400

    fix: update BasePage and HomePage

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,10 +1,13 @@
 package pages;
 
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.ConfigReader;
 
 public abstract class BasePage {
     protected WebDriver driver;
@@ -24,14 +27,31 @@ public abstract class BasePage {
 
     /**
      * Waits for the page to fully load.
-     * Must be implemented by each specific page.
      */
     @Step("Wait for page to load")
-    protected abstract void waitForPageToLoad();
+    protected void waitForPageToLoad(By element) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(element));
+    };
 
     /**
-     * Returns the URL for the specific page. This must be implemented in each subclass.
+     * Returns the URL for the specific page.
      */
-    protected abstract String getUrl();
+    protected String getUrl(String url) {
+        return ConfigReader.getConfigProperty(url);
+    };
+
+    /**
+     * Retrieves the current page title.
+     * This method is commonly used in tests for validation purposes,
+     * and can be reused across all page classes that extend BasePage.
+     *
+     * @return The title of the currently loaded page.
+     */
+    @Step("Getting the current page title")
+    public String getTitle() {
+        String title = driver.getTitle();
+        logger.info("Current page title: {}", title);
+        return title;
+    }
 }
 
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -3,9 +3,7 @@ package pages;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import utils.ConfigReader;
 
 public class HomePage extends BasePage {
 
@@ -15,31 +13,13 @@ public class HomePage extends BasePage {
         super(driver, wait);
     }
 
-    @Override
-    protected String getUrl() {
-        // Get the base URL directly from the config file
-        return ConfigReader.getConfigProperty("baseUrl");
-    }
-
-    @Override
-    @Step("Waiting for Home Page to load")
-    protected void waitForPageToLoad() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(logo));
-    }
-
     @Step("Opening Home Page using URL from config")
     @Override
     public void open() {
-        String url = getUrl();
+        String url = getUrl("baseUrl");
         logger.info("Opening Home Page at URL: {}", url);
         driver.get(url);
-        waitForPageToLoad();
+        waitForPageToLoad(logo);
     }
 
-    @Step("Getting the current page title")
-    public String getTitle() {
-        String title = driver.getTitle();
-        logger.info("Current page title: {}", title);
-        return title;
-    }
 }
